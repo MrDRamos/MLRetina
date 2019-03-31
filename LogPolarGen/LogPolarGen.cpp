c3102ce815a1ce2a6451b4168e6692118215e55d
dramos <david.ramos@openlink.com>
2019-03-31 00:15:23 +0000

    msbuild changes & .gitignore file

--- LogPolarGen/LogPolarGen.cpp
+++ LogPolarGen/LogPolarGen.cpp
@@ -33,7 +33,7 @@ public:
 	{
 		nR = std::max(nR, 1);
 		nA = std::max(nA, 1);
-		float nW = (nA + 1) * (nR + 1);
+		float nW = (float)(nA + 1) * (nR + 1);
 		std::map<std::tuple<int16_t, int16_t>, uint16_t> CountS;
 
 		float dR = (Rmax - Rmin) / nR;
@@ -42,10 +42,12 @@ public:
 		for (int ia = 0; ia <= nA; ++ia, A += dA)
 		{
 			float R = Rmin;
+			float cosA = cos(A);
+			float sinA = sin(A);
 			for (int ir = 0; ir <= nR; ++ir, R += dR)
 			{
-				int16_t x = std::round(R * cos(A));
-				int16_t y = std::round(R * sin(A));
+				int16_t x = (int16_t)std::round(R * cosA);
+				int16_t y = (int16_t)std::round(R * sinA);
 				auto xy = std::make_tuple(x, y);
 				++CountS[xy];
 			}
@@ -71,21 +73,21 @@ public:
 
 int main()
 {
-	int Rn = 400; // Radia
-	int An = 360; // angles
-	float Arc = 2.0 * pi / An;
+	uint16_t Rn = 400; // Radia
+	uint16_t An = 360; // angles
+	float Arc = 2.0f * (float)pi / An;
 	std::vector<GridPtSource> GridPointS;
 	GridPointS.reserve(Rn * An * 10);
 
-	for (int ri = 0; ri < Rn; ri++)
+	for (uint16_t ri = 0; ri < Rn; ri++)
 	{
-		float Rmin = ri;
-		float Rmax = ri + 1;
-		int nR = 3;
-		int nA = std::round(Arc * Rmax * 3);
-		for (int ai = 0; ai < An; ai++)
+		float Rmin = (float)ri;
+		float Rmax = (float)(ri + 1);
+		uint16_t nR = 3;
+		uint16_t nA = (uint16_t)std::round(Arc * Rmax * 3);
+		for (uint16_t ai = 0; ai < An; ai++)
 		{
-			float Amin = ai;
+			float Amin = (float)ai;
 			float Amax = Amin + Arc;
 			auto gpS = WedgeRoi::GetGridPt(Rmin, Rmax, nR, Amin, Amax, nA);
 			GridPointS.insert(GridPointS.end(), gpS.begin(), gpS.end());
